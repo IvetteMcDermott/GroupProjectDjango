318aac426dd89db7dfea812ff053a3d8cf0f74fa
IvetteMcDermott <ivettedemcdermott@gmail.com>
2023-03-03 13:14:13 +0000

    Fix in allowed hosts

--- group_blog/settings.py
+++ group_blog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-group-project.herokuapp.com', 'local']
+ALLOWED_HOSTS = ['django-group-project.herokuapp.com', 'localhost']
 
 
 # Application definition
