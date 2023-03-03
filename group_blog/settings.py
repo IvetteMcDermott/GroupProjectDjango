4848f1e9b007e29b0765c681fe99e48478f74f6d
IvetteMcDermott <ivettedemcdermott@gmail.com>
2023-03-03 12:49:23 +0000

    Add allowed host to settings

--- group_blog/settings.py
+++ group_blog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://django-group-project.herokuapp.com/', 'local']
 
 
 # Application definition
