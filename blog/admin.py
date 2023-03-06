4c554f379224ccd28bb3aac4e50a2d8815849220
IvetteMcDermott <ivettedemcdermott@gmail.com>
2023-03-06 17:51:44 +0000

    Fix issues in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -7,22 +7,22 @@ from .models import Spice, Comment, TypeCategory
 
 @admin.register(TypeCategory)
 class TypeCategory(admin.ModelAdmin):
-    list_filter = ('category')
+    list_filter = ('category',)
     search_fields = ['category',]
-    list_display = ('category')
+    list_display = ('category',)
 
 
 @admin.register(Spice)
 class Spice(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
-    list_filter = ('name', 'type_category', 'uses_category',)
-    search_fields = ['name', 'type_category', 'uses_category',]
-    list_display = ('name', 'type_category', 'uses_category', 'image',
-                    'description', 'price', 'bookmarks', 'price')
+    list_filter = ('name', 'type_category', 'use_category',)
+    search_fields = ['name', 'type_category', 'use_category',]
+    list_display = ('name', 'use_category', 'image',
+                    'description', 'price', 'price')
 
 
 @admin.register(Comment)
 class Comment(admin.ModelAdmin):
     list_filter = ('author', 'spice')
     search_fields = ['author', 'spice']
-    list_display = ('author', 'spice', 'body', 'date_created')
+    list_display = ('author', 'spice', 'body', 'date_created',)
